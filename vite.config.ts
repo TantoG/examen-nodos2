f61dc1c3a2182c224c2227d6c729aab7276d471b
Juan Herrera <73452788+TantoG@users.noreply.github.com>
2025-10-21 14:05:24 -0300

    Fix GitHub Pages 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/examen-nodos2/" : "/",
+  base: mode === "production" ? "/pastel-node-flow/" : "/",
   server: {
     host: "::",
     port: 8080,
